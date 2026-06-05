e991bd69f8f7cea992304043865bffca17468f4b
rite2harini <rite2harini@gmail.com>
2026-06-05 14:12:55 +0530

    Create Student_Marks.java for marks storage and display
    Implement a student marks system with input validation.

---
+++ Student_Marks.java
@@ -0,0 +1,12 @@
+/*
+Student Marks System (Safe Input)
+
+Create a program that:
+
+Stores 5 student marks in an array
+Takes index input from user
+Displays mark
+Add Exception Handling:
+Handle invalid index
+Handle invalid input (if user enters text instead of number)
+*/
